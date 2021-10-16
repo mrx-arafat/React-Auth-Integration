3b8badb510a776328c09bcdeb29a957a6e31a270
Easin Arafat <arafatmrx@gmail.com>
2021-10-16 20:20:06 +0600

    firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import useAuth from '../../hooks/useAuth';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    const { signInUsingGoogle, signInUsingGithub } = useAuth();
-    return (
-        <div>
-            <h2>Please Login</h2>
-            <button onClick={signInUsingGoogle}>Google Sign In</button>
-            <br />
-            <button onClick={signInUsingGithub}>GitHub Sign In</button>
-            <br />
-            <Link to="/register">New User?</Link>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Please Login</h2>
+      {/* <button onClick={signInUsingGoogle}>Google Sign In</button>
+      <br />
+      <button onClick={signInUsingGithub}>GitHub Sign In</button>
+      <br /> */}
+      <Link to="/register">New User?</Link>
+    </div>
+  );
 };
 
 export default Login;
